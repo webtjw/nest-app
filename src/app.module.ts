33cb46c158df59701ff31ba1b9693aa7526ff53c
webtjw <webtjw@foxmail.com>
2018-07-25 22:20:09 +0800

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BlogModule } from "./modules/blog/blog.module";
 import { LoggerMiddleware } from 'middlewares/logger.middleware';
 import { BlogController } from 'modules/blog/blog.controller';
 
 @Module({
-  imports: [BlogModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BlogModule]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes(AppController, BlogController);
+    consumer.apply(LoggerMiddleware).forRoutes(BlogController);
   }
 }
